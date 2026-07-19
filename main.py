aafed1a5f644b9ed7f348e5cecc18607b44b44c2
Mahiro <2019ng0616@gmail.com>
2026-07-20 00:52:20 +0900

    feat: support GitHub Pages

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from shutil import copy2
+import os
+
 from news.fetch import fetch_all_feeds
 from news.save import save_to_json
 from generate_html import generate_html
@@ -13,8 +16,21 @@ def main():
 
     generate_html(articles)
 
-    print(f"Saved {len(articles)} articles.")
-    print("Done.")
+    os.makedirs("docs", exist_ok=True)
+
+    copy2(
+        "output/index.html",
+        "docs/index.html"
+    )
+
+    copy2(
+        "style.css",
+        "docs/style.css"
+    )
+
+    print()
+
+    print("GitHub Pages updated.")
 
 
 if __name__ == "__main__":
